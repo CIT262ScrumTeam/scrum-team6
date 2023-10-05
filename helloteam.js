cf84c04a947601d0b2e7df5bbaf1af28ee4eee77
mattmann24 <mdshank24@gmail.com>
2023-10-04 19:53:17 -0600

    Matt Shank Added

--- helloteam.js
+++ helloteam.js
@@ -1,5 +1,6 @@
 const teamNames = [];
 teamNames.push("Josh Dalton")
+teamNames.push("Matthew Shank")
     for (let i = 0; i < teamNames.length; i++) {
         console.log("Hello " + teamNames[i])
     }
