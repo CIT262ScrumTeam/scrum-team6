543dbf6e1840240d6f8edc97dee796e7006332b7
Walter Taylor <walterht@byui.edu>
2023-10-12 21:49:18 -0600

    changed for loop to foreach loop

--- helloteam.js
+++ helloteam.js
@@ -2,6 +2,5 @@ const teamNames = [];
 teamNames.push("Josh Dalton")
 teamNames.push("Matthew Shank")
 teamNames.push("Walter Taylor")
-    for (let i = 0; i < teamNames.length; i++) {
-        console.log("Hello " + teamNames[i])
-    }
+    
+teamNames.forEach((element) => console.log(`Hello ${element}`))
