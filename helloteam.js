21f661306accaef230ba401b78e255ab398960ce
Walter Taylor <walterht@byui.edu>
2023-10-12 21:45:27 -0600

    Add Walter name to array

--- helloteam.js
+++ helloteam.js
@@ -1,6 +1,7 @@
 const teamNames = [];
 teamNames.push("Josh Dalton")
 teamNames.push("Matthew Shank")
+teamNames.push("Walter Taylor")
     for (let i = 0; i < teamNames.length; i++) {
         console.log("Hello " + teamNames[i])
     }
